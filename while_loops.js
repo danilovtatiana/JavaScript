e832f18a234795f38cd7ee99ce437375b234a580
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-01-31 21:51:34 +0200

    while loop, print even numbers

---
+++ while_loops.js
@@ -0,0 +1,23 @@
+// var x = 0;
+//
+// while (x < 5) {
+//   console.log("x is currently: " + x);
+//
+//   if (x === 3) {
+//     console.log("X IS EQUAL TO THREE!")
+//   }
+//   console.log("x is still less than 5, adding 1 to x");
+//   x++;
+// }
+
+
+//write a while loop that prints out only the even numbers from 1 to 10
+
+var num = 0;
+
+while (num < 11) {
+  if (num % 2 === 0) {
+    console.log (num)
+  }
+  num++;
+}

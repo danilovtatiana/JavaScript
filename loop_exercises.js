e7e28c214f0c3b8249bca627d7f05da97f555e80
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-01-31 22:46:02 +0200

    print out "hello" 5 times

---
+++ loop_exercises.js
@@ -0,0 +1,18 @@
+//Use a For Loop to print out the word "hello" 5 times
+
+//while loop
+
+
+x = 0;
+
+while (x < 5) {
+  console.log("hello");
+
+  x++;
+}
+
+//for loop
+
+for (var i = 0; i < 5; i++) {
+  console.log("hello");
+}

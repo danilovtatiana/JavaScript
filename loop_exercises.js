41f774044a9d2ea4f1ac3a68adb22e9c4da134fd
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-01-31 22:47:09 +0200

    Update loop_exercises.js
    print all odd numbers from 1 to 25

--- loop_exercises.js
+++ loop_exercises.js
@@ -16,3 +16,24 @@ while (x < 5) {
 for (var i = 0; i < 5; i++) {
   console.log("hello");
 }
+
+//Use loops to print out all odd numbers from 1 to 25
+
+//while loop
+
+num = 1;
+
+while (num < 26) {
+  if (num % 2 !== 0) {
+    console.log(num);
+  }
+  num++;
+}
+
+//for loop
+
+for (var i = 0; i < 26; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}

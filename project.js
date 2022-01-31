13980ba709f36138107b23b4f659ded1c8c204ab
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-01-31 23:16:34 +0200

    set the variables

---
+++ project.js
@@ -0,0 +1,10 @@
+var firstName = prompt("What is your First Name?");
+var lastName = prompt("What is your Last Name?");
+var age = prompt("How old are you?");
+var height = prompt("What is your height?");
+var pet = prompt("What is your pet's name?");
+
+var nameCond = null;
+var ageCond = null;
+var heightCond = null;
+var petCond = null;

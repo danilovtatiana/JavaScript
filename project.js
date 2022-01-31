cbab63fad04762cfae134722caee37b410a4b634
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-01-31 23:24:26 +0200

    Update project.js
    checked all conditions

--- project.js
+++ project.js
@@ -8,3 +8,43 @@ var nameCond = null;
 var ageCond = null;
 var heightCond = null;
 var petCond = null;
+
+//NAME CONDITION
+
+if (firstName[0] === lastName[0]) {
+  nameCond = true;
+} else {
+  nameCond = false;
+}
+
+//AGE CONDITION
+
+if (age >= 20 && age <=30) {
+  ageCond = true
+}else {
+  ageCond = false;
+}
+
+//HEIGHT CONDITION
+
+if (height >= 170) {
+  heightCond = true;
+}else {
+  heightCond = false;
+}
+
+//PET NAME CONDITION
+
+if (pet[pet.length-1] === "y") {
+  petCond = true;
+}else {
+  petCond = false;
+}
+
+//ALL CONDITIONS
+
+if (nameCond && ageCond && heightCond && petCond) {
+  console.log("You are a Spy!");
+} else {
+  console.log("You are not a Spy!");
+}

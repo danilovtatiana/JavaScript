d230394dc523f6fee1d94a3484edcf4f76004168
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-01-31 21:02:56 +0200

    Update exercise_2.js
    converted lbs into kg

--- exercise_2.js
+++ exercise_2.js
@@ -1,1 +1,5 @@
-
+alert("Let's create a convertor from lbs to kg!");
+var pounds = prompt("How many pounds do you have? ");
+var kg = pounds * 0.454;
+alert("It means that you have: " + kg + "kg");
+console.log("Conversion Completed");

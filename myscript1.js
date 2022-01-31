022edc9236c4cd29a123f2e4f86ed345d70dcff9
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-01-31 20:24:26 +0200

    Update myscript1.js
    variable, alert, prompt and console.log

--- myscript1.js
+++ myscript1.js
@@ -1,1 +1,4 @@
-alert("Hi")
+alert("Welcome to your bank");
+var deposit = prompt("How much would you like to deposit?");
+alert("You've deposited: " + deposit);
+console.log("You are a cool person!");

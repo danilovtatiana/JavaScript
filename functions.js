2fb2a6670a459a57023b9bf98d1340197c5eb62d
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-02-07 15:36:31 +0200

    Update functions.js
    function with one parameter

--- functions.js
+++ functions.js
@@ -1,3 +1,7 @@
-function hello (){
+function hello (){  //simple function
   console.log("hello world!");
+}
+
+function helloYou(name){ //function with one parameter
+  console.log("Hello "+name);
 }

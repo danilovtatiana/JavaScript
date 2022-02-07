2df3c32fc124f737984c3d2d8cc3c51f03b4f2f8
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-02-07 17:08:58 +0200

    Update functions_exercises.js
    lucky sum

--- functions_exercises.js
+++ functions_exercises.js
@@ -43,3 +43,22 @@ function stringTimes(str,n) {
   }
   return returnStr
 }
+
+//Problem 4: LUCKY SUM
+
+//Given 3 numerical values, a b c, return their sum. However is one of the value
+// 13 then it does not count towards the sum and values to its right do not count.
+//So for example, if b is 13, then both b and c do not count
+
+
+function luckySum(a,b,c){
+  if (a === 13) {
+    return 0
+  }else if (b === 13) {
+    return a
+  }else if (c === 13) {
+    return a + b
+  }else {
+    return a+b+c
+  }
+}

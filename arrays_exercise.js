6fb00bd818ccc563e326737f12a9c442816e5db7
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-02-07 19:35:10 +0200

    create the functions for the tasks

---
+++ arrays_exercise.js
@@ -0,0 +1,31 @@
+// create empty student roster array
+
+var roster = []
+
+//create the functions for the tasks
+
+//ADD A NEW STUDENT
+
+//Create a function called addNew that takes is a name and uses .push to add a new student to the array
+
+function addNew(){
+  var newName = prompt("What name would you like to add?")
+   roster.push(newName)
+}
+
+//REMOVE THE STUDENT
+
+//Create a function called remove that takes in a name, finds its index location and then removes the name from the roster
+
+function remove(){
+  var remName = prompt("What name would you like to remove?")
+  var index = roster.indexOf(remName);
+  roster.splice(index,1)
+}
+//DISPLAY ROSTER
+
+//Create a function called display that prints out the roster to the console
+
+function display(){
+  console.log(roster);
+}

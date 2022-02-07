2181e572fe1201515ad96b76e23045e376aa3ec6
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-02-07 16:41:01 +0200

    Update functions_exercises.js
    sleepIn()

--- functions_exercises.js
+++ functions_exercises.js
@@ -6,16 +6,16 @@
 //We sleep in if it is not a weekday or we are on vacation. Return true if we sleep in.
 
 // 1st variant
-function sleepIn(weekday, vacation){
-  if ((weekday !== true) || (vacation == true)) {
-    return true
-  }else {
-    return false
-  }
-}
+// function sleepIn(weekday, vacation){
+//   if ((weekday !== true) || (vacation == true)) {
+//     return true
+//   }else {
+//     return false
+//   }
+// }
 
 //2nd
 
-function sleepIn(weekday, vacation) {
-  return (!weekday || vacation)
-}
+// function sleepIn(weekday, vacation) {
+//   return (!weekday || vacation)
+// }

d179f1fe886f01118234d87c7b66b135f5abac96
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-02-07 15:43:12 +0200

    Update functions.js
    function with two parameters

--- functions.js
+++ functions.js
@@ -5,3 +5,7 @@ function hello (){  //simple function
 function helloYou(name){ //function with one parameter
   console.log("Hello "+name);
 }
+
+function addNum(num1, num2){ // function with two parameters
+  console.log(num1+num2);
+}

f9df4a06895e478f59776009fbc6e7150cc05cc6
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-02-08 12:38:58 +0200

    Update objects_exercises.js
    problem 3, split a string in array

--- objects_exercises.js
+++ objects_exercises.js
@@ -5,14 +5,20 @@ var employee = {
   nameLength: function(){  //Problem 1
     console.log(this.name.length - 1);
   },
-  report: function(){
+  report: function(){ //Problem 2
     alert("Name is "+ this.name + ", Job is " + this.job + ", Age is " + this.age)
+  },
+  lastName: function(){
+    console.log(this.name.split(" ")[1]);
   }
 }
 
-//add a method called  nameLength that prints out the name of the employees name to the console.
+//Problem 1: add a method called  nameLength that prints out the name of the employees name to the console.
 
 
-//write program that will create an Alert in the browser of each of the object's
+//Problem 2: write program that will create an Alert in the browser of each of the object's
 //values for the key value pairs. For example, it should alert:
 //Name is John Smith, Job is Programmer, Age is 31
+
+
+// Problem 3: add a method called lastName that prints out the employee's last name to the console.

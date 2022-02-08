86237b36173144c4986a1187c5f650bc778268dc
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-02-08 12:06:33 +0200

    Update objects.js
    object methods

--- objects.js
+++ objects.js
@@ -1,6 +1,9 @@
 // create an object
 
-var carInfo = {make:"Toyota", year: 1990, model:"Camry"}
+var carInfo = {make:"Toyota", year: 1990, model:"Camry", carAlert: function(){
+  alert("We've got a car here!")
+}
+};
 
 //object with different data types
 
@@ -15,5 +18,29 @@ carInfo['year'] += 1;  //2007
 //itereting through an object (for in)
 for (key in carInfo){
   console.log(key); //make, year, model
-  console.log(carInfo[key]);
+  console.log(carInfo[key]); //values !could be in different order
+}
+
+//OBJECT METHODS - functions inside of the objects
+
+var myObj = {
+  prop:37,
+  reportProp: function () {
+    return this.prop;
+  }
+};
+console.log(myObj.reportProp());
+
+var simple = {
+  prop: "Hello",
+  myMethod: function(){
+    console.log("The myMethod was called");
+  }
+}
+
+myObject = {
+  name: "Jose",
+  greet: function(){
+    console.log("Hello " + this.name);
+  }
 }

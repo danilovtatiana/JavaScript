3b8381f2e9c7b7869fe86ccdb76198beaf01f1ff
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-02-08 12:30:03 +0200

    Update objects_exercises.js
    Problem 2
    create an Alert in the browser of each of the object's values for the key value pairs

--- objects_exercises.js
+++ objects_exercises.js
@@ -2,10 +2,17 @@ var employee = {
   name: "John Smith",
   job: "Programmer",
   age: 31,
-  nameLength: function(){
+  nameLength: function(){  //Problem 1
     console.log(this.name.length - 1);
+  },
+  report: function(){
+    alert("Name is "+ this.name + ", Job is " + this.job + ", Age is " + this.age)
   }
 }
 
-
 //add a method called  nameLength that prints out the name of the employees name to the console.
+
+
+//write program that will create an Alert in the browser of each of the object's
+//values for the key value pairs. For example, it should alert:
+//Name is John Smith, Job is Programmer, Age is 31

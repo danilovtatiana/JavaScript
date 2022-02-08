4991e68c8291e5c0986b7f0c78b4233e6a5a225f
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-02-08 12:19:54 +0200

    Problem 1
    add a method called  nameLength that prints out the name of the employees name to the console.

---
+++ objects_exercises.js
@@ -0,0 +1,11 @@
+var employee = {
+  name: "John Smith",
+  job: "Programmer",
+  age: 31,
+  nameLength: function(){
+    console.log(this.name.length - 1);
+  }
+}
+
+
+//add a method called  nameLength that prints out the name of the employees name to the console.
